68e64278b319fae9f2c7a3078f42242b28f42e97
stereoIII6 <stereodocbush@gmail.com>
2022-06-17 10:46:54 +0200

    mercury

--- migrations/2_deploy_s0x.js
+++ migrations/2_deploy_s0x.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
-
-// const s0xiety = artifacts.require("s0xiety");
-const Affilly8 = artifacts.require("Affilly8");
+const Gardens = artifacts.require("Gardens.sol");
 
 
 module.exports = function (deployer) {
-  // deployer.deploy(s0xiety, process.env.ADMIN);
-  deployer.deploy(Affilly8);
-
+  deployer.deploy(Gardens);
 };
 
